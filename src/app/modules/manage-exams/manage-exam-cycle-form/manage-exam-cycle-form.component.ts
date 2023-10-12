ee0a5a43fd6971ef5fa37952f3c20b6c5f1ef06b
Roopashree <roopashree.lakshmikeshava@tarento.com>
2023-10-12 11:41:41 +0530

    added subject dropdown

--- src/app/modules/manage-exams/manage-exam-cycle-form/manage-exam-cycle-form.component.ts
+++ src/app/modules/manage-exams/manage-exam-cycle-form/manage-exam-cycle-form.component.ts
@@ -298,8 +298,7 @@ export class ManageExamCycleFormComponent {
     this.subjects = [];
     this.baseService.getSubjectsByCourse(courseId).subscribe({
       next: (res) => {
-        this.subjects = res.responseData;
-        console.log(this.subjects);
+        this.subjects = res.responseData.subjects;
       }
     })
   }
